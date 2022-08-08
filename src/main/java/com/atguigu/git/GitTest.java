8513fd7eae89dfa73bea7c0df5b553a3c917daac
wlw <wlw@1763714.com>
2022-08-09 02:37:29 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wlwstart
+ * @create 2022-08-09 2:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
